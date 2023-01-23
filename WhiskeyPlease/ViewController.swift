f2af788011f8ac9150511904520a543c7758fe39
JakubPrusGithub <bluerbluerbluer@gmail.com>
2023-01-24 00:45:35 +0100

    Table view template

--- WhiskeyPlease/ViewController.swift
+++ WhiskeyPlease/ViewController.swift
@@ -11,17 +11,15 @@ class ViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        overrideUserInterfaceStyle = .dark
         navigationController?.navigationBar.prefersLargeTitles = true
         title="Articles"
+        tableView.separatorColor = UIColor(named: "textColor")
         
         let gradientLayer = CAGradientLayer()
         gradientLayer.frame = tableView.bounds
         let myGrayLighter = UIColor(red: 30.0/255.0, green: 30.0/255.0, blue: 30.0/255.0, alpha: 1.0)
         let myGrayDarker = UIColor(red: 15.0/255.0, green: 15.0/255.0, blue: 10.0/255.0, alpha: 1.0)
-//        gradientLayer.colors = [
-//            UIColor.black.cgColor,
-//            myGray.cgColor
-//        ]
         gradientLayer.colors = [
             myGrayDarker.cgColor,
             myGrayLighter.cgColor
@@ -35,13 +33,11 @@ class ViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Article", for: indexPath) as! ArticleTableViewCell
+        
         cell.title.text = "Default title"
         cell.thumbnail.image = UIImage()
-        cell.desc.text = "Default, short and simple description provided from application for this specific cell of table for article"
+        cell.desc.text = "Default, short and simple description provided from application for this specific cell of table for article. Default, short and simple description provided from application for this specific cell of table for article."
         cell.backgroundColor = .clear
-        //let descColor = UIColor(red: 200.0/255.0, green: 200.0/255.0, blue: 200.0/255.0, alpha: 1.0)
-        let descColor = UIColor(red: 140.0/255.0, green: 130.0/255.0, blue: 110.0/255.0, alpha: 1.0)
-        let titleColor = UIColor(red: 220.0/255.0, green: 200.0/255.0, blue: 150.0/255.0, alpha: 1.0)
         let thumbnail = UIImage(named: "default.jpeg")
         cell.title.textColor = UIColor(named: "titleColor")
         cell.desc.textColor = UIColor(named: "textColor")
@@ -52,7 +48,7 @@ class ViewController: UITableViewController {
         return 200
     }
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return 15
     }
 }
 
